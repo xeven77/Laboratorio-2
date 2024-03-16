42ac2e0e8d1290f8c0d02efb46696345817ff399
JuanMCelis <163361602+JuanMCelis@users.noreply.github.com>
2024-03-16 11:56:27 -0500

    Aplicacion 1 Clase 16/03/2024

---
+++ APLICACION 1/JS/forms.js
@@ -0,0 +1,21 @@
+//const formulario = document.forms['formulario']; 
+const agregarbtn = document.getElementById('agregarbtn')
+const eliminarbtn = document.getElementById('eliminarbtn')
+const modificarbtn = document.getElementById('modificarbtn')
+
+
+
+//metodos
+
+
+agregarbtn.addEventListener('click',()=>{
+    
+})
+
+eliminarbtn.addEventListener('click',()=>{
+
+})
+
+modificarbtn.addEventListener('click',()=>{
+
+})
